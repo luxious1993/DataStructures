cc8090e2d15607f938aecb5933c53f66883f21f5
luxious <liuxinlove1993@gmail.com>
2019-03-30 05:31:41 -0700

    stackSort

--- src/DoubleStackSort.java
+++ src/DoubleStackSort.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 
 public class DoubleStackSort {
     //assume no duplicate num
-    public static LinkedList<Integer> doubleStackSort(LinkedList<Integer> stack1) {
+    public static LinkedList<Integer> doubleStackSortNoDup(LinkedList<Integer> stack1) {
         LinkedList<Integer> stack2 = new LinkedList<>();
         while (!stack1.isEmpty()) {
             int globalMin = stack1.peekFirst();
@@ -23,12 +23,41 @@ public class DoubleStackSort {
         }
         return stack2;
     }
+    public static LinkedList<Integer> doubleStackSort(LinkedList<Integer> stack1) {
+        LinkedList<Integer> stack2 = new LinkedList<>();
+        while (!stack1.isEmpty()) {
+            int globalMin = stack1.peekFirst();
+            int count = 0;
+            while (!stack1.isEmpty()) {
+                int num = stack1.pollFirst();
+                if (num < globalMin) {
+                    globalMin = num;
+                    count = 1;
+                } else if (num == globalMin) {
+                    count++;
+                }
+                stack2.offerFirst(num);
+            }
+            while (!stack2.isEmpty() && stack2.peekFirst() >= globalMin) {
+                int num = stack2.pollFirst();
+                if (num > globalMin) {
+                    stack1.offerFirst(num);
+                }
+            }
+            while (count > 0) {
+                stack2.offerFirst(globalMin);
+                count--;
+            }
+        }
+        return stack2;
+    }
     public static void main(String[] args) {
         LinkedList<Integer> stack1 = new LinkedList<>();
         stack1.offerFirst(2);
         stack1.offerFirst(1);
         stack1.offerFirst(3);
-        stack1.offerFirst(4);
+        stack1.offerFirst(1);
+        stack1.offerFirst(2);
         LinkedList<Integer> res = doubleStackSort(stack1);
         while (!res.isEmpty()) {
             System.out.println(res.pollFirst());
